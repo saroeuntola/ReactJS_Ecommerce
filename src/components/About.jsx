b0389b533e9ed5477390c28b7a1d6b9af3d35110
SaroeunTola <tolasweatboy@gmail.com>
2025-01-04 10:13:20 +0700

    update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FaHeadset, FaThumbsDown, FaThumbsUp, FaTruck } from "react-icons/fa";
+import { FaHeadset, FaThumbsUp, FaTruck } from "react-icons/fa";
 const About = () => {
     const [loading,setLoading] = useState (true)
     useEffect(() => {
@@ -8,7 +8,7 @@ const About = () => {
         }, 700)
     }, [])
   return (
-    <main className=" flex justify-center py-40 md:mb-96">
+    <main className=" flex justify-center py-40">
       {loading ? (
         <div className="animate-spin border-t-4 border-blue-500 border-solid rounded-full w-16 h-16 mt-32"></div>
       ) : (
